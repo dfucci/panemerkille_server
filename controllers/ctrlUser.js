e43353b2856bee318e32f8d83d41f84f020d15b7
slacko <ancestrall@gmail.com>
2012-03-12 20:22:41 +0200

    aggiunta test suite

--- controllers/ctrlUser.js
+++ controllers/ctrlUser.js
@@ -1,5 +1,8 @@
-//TODO validazione
-
+/*TODO validazione
+- aggiundere la risposta in POST nell'header 'Location' e codice 201
+- 
+*/
+var _ = require('../libs/underscore')._;
 var User = require('../models/user.js');
 UserController = function(){};
 
@@ -7,7 +10,7 @@ exports.getUsers =function(req, res) {
 	User.find({}, function (err, docs) {
 			if(!err){
 				res.send(docs);
-			}
+			} else res.send(err);
 		});	
 	}
 
@@ -22,17 +25,18 @@ var user = new User({
 		facebook_id:req.body.facebook_id,
 		email:req.body.email
 	});
+
 	user.save(function(err) {
 		if(!err){
 			res.send(req.url+'/'+user._id);
-		}
+		} else res.send(err);
 	});
 }
 exports.delUsers=function (req,res) {
 	User.remove({}, function  (err) {
 		if(!err){
 			res.send(req.url);
-		}
+		} else res.send(err);
 	});
 }
 
@@ -42,12 +46,22 @@ exports.getUser = function(req, res) {
 	User.findOne({_id:_id}, function (err, doc) {
 		if(!err){
 			res.send(doc);
-		}
+		} else res.send(404, req.url+" not found");
 	});	
 }
 
 exports.putUser=function (req, res) {
- 	_id=req.params._id;
+	var myparams=['name',
+		'surname',
+		'bithdate',
+		'gender',
+		'picture_url',
+		'facebook_id',
+		'email'];
+
+	console.log('start');
+if(checkParams(myparams, req)){
+	console.log('params ok');
 	User.update({_id:_id}, {$set:{
 		name : req.body.name,
 		surname:req.body.surname,
@@ -59,10 +73,13 @@ exports.putUser=function (req, res) {
 	}}, {upsert: true},function  (err) {
 		if (!err){
 			res.send(req.url);
-		}
+		} else res.send(404, req.url+" not found");
 		
 	});	
-
+	} else {
+		console.log('params not ok');
+		res.send(400, 'Required parameter missing');
+	}
 }
 
 exports.delUser=function (req, res) {
@@ -70,8 +87,24 @@ exports.delUser=function (req, res) {
 	User.remove({_id:_id}, function (err, doc) {
 		if(!err){
 			res.send(req.url.substring(0, req.url.length-_id.length-1));	
-		}	
+		} else res.send(404, req.url+" not found");
 	});	
 }
 
+
 exports.UserController=UserController;
+
+function checkParams (params, req) {
+	console.log('checking');
+	for (var i = 0 ; i < params.length; i++) {
+		console.log(i);
+		if(!(_.include(req.params, params[i]))){
+			console.log('false');
+			return false;
+			}
+
+		}
+
+		if (req.body.params.length>1) return true;
+		else return false;
+}
---
+++ tests/user-test.js
@@ -0,0 +1,73 @@
+/*TODO
+-test PUT con id sbagliato 404
+*/
+var User = require('../models/user.js');
+var db = require('mongoose');
+db.connect('mongodb://localhost/panemerkilledb');
+var APIeasy = require('api-easy'),
+    assert = require('assert');
+
+var suite = APIeasy.describe('/users');
+suite.use('localhost', 7777);
+
+suite.discuss('When using the User resource')
+suite.before('set up the context', function(ongoing) {
+    User.remove({}, function  (err) {
+      if(!err) console.log('ok');
+      else console.log(err);
+    });
+    suite.setHeader('Content-Type', 'application/json')
+    return ongoing;
+});
+
+suite.discuss('When testing error codes')
+  .put('/users/michele').expect(400)
+  .put('/users').expect(405)
+  .post('/users/michele').expect(405)
+  .get('/users/michele').expect(404)
+  .put('/users/michele', {
+    name:'Davide',
+    surname:'F',
+    birthdate:'06/16/1985',
+    gender:'male',
+    picture_url:'none',
+    facebook_id:'Aldo',
+    email:'mia@mio.fi'
+  }).expect(404)
+.undiscuss();
+
+suite.discuss('When testing GET /users')
+.setHeader('Content-Type', 'application/json')
+.get('/users').expect(200)
+.expect('should respond with a not null JSON object', function(err, res, body) {
+  console.log('testing "should respond with a not null JSON object"');
+  var result = JSON.parse(body);
+  assert.isNotNull(result);
+
+})
+.expect('should return an empty JSON array', function  (err, res, body) { 
+  console.log('testing "should return an empty JSON array"');
+    
+    var results = JSON.parse(body);
+   assert.isEmpty(results);
+})
+.undiscuss();
+
+suite.discuss('When testing POST /users')
+.post('/users', {
+    name:'Davide',
+    surname:'F',
+    birthdate:'06/16/1985',
+    gender:'male',
+    picture_url:'none',
+    facebook_id:'Aldo',
+    email:'mia@mio.fi'
+  })
+.expect('should return a not null JSON object', function  (err, res, body) {
+  console.log('testing "should return a not null JSON object"');
+  var result = JSON.parse(body);
+  assert.isNotNull(result);
+})
+.undiscuss()
+.undiscuss()
+.export(module);
