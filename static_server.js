70b7d04f18f18a452573c903f9416a8065c20c23
slacko <ancestrall@gmail.com>
2012-04-05 15:32:27 +0300

    static

--- static_server.js
+++ static_server.js
@@ -1,7 +1,8 @@
 var fileServer = require('node-static');
 var file = new(fileServer.Server) ('./static');
+var port = process.env.PORT || 7778;
 require('http').createServer(function(req, res) {
 	req.addListener('end', function() {
 		file.serve(req, res)
 	});
-}).listen(7778);
+}).listen(port);
