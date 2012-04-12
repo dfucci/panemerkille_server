49c74da71be9ea521a0332c16de9bfe03fbf7d6d
slacko <ancestrall@gmail.com>
2012-04-12 20:10:05 +0300

    removed static

--- static_server.js
+++
@@ -1,8 +0,0 @@
-var fileServer = require('node-static');
-var file = new(fileServer.Server) ('./static');
-var port = process.env.PORT || 7778;
-require('http').createServer(function(req, res) {
-	req.addListener('end', function() {
-		file.serve(req, res)
-	});
-}).listen(port);
