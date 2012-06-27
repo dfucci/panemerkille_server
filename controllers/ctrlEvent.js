f11c17cdcdbe783972c083097f23f4a6cbecb240
slacko <ancestrall@gmail.com>
2012-06-27 20:46:58 +0300

    check starting events date

--- controllers/ctrlEvent.js
+++ controllers/ctrlEvent.js
@@ -16,7 +16,7 @@ exports.getEvents = function(req, res) {
 		end = new Date();
 		end.setDate(end.getDate() + 7);
 	}
-	Event.find({$or:[{'time.end':{$gte:start}, 'time.end':{$lte:end}},{'time.start':{$lte:start}, 'time.st'}).sort('time.start', 1).populate('venue').exec(function(err, events){
+	Event.find({$or:[{'time.end':{$gte:start}, 'time.end':{$lte:end}},{'time.start':{$lte:start}, 'time.start':{$gte:end}}).sort('time.start', 1).populate('venue').exec(function(err, events){
 		if (err) {
 			res.send(500, err);
 		}else{
