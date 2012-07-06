9e13bfdfe2d3ab051f6b190751c434b5ead6791e
slacko <ancestrall@gmail.com>
2012-07-06 12:00:14 +0300

    last checkin

--- controllers/ctrlUser.js
+++ controllers/ctrlUser.js
@@ -189,7 +189,7 @@ exports.getUserFriends = function(req, res) {
 			console.log(user.friends[i].friend);
 			User.findOne({
 				_id: user.friends[i].friend
-			}).populate('checkins.event').exec(function(err, friend) {
+			}).populate('checkins.event',{},{},{limit:1}).exec(function(err, friend) {
 				if (err) res.send(500, err);
 				else {
 					output.push(friend);
