e2d50955250486385ef7b08503b4603ffee32e11
slacko <ancestrall@gmail.com>
2012-08-28 18:26:25 +0300

    fixed sort in Mongoose 3.x

--- controllers/ctrlEvent.js
+++ controllers/ctrlEvent.js
@@ -20,7 +20,7 @@ exports.getEvents = function(req, res) {
 		oneweek = new Date();
 		oneweek.setDate(oneweek.getDate() + 7);
 	}
-	Event.find({$or:[{'time.end':{$gte:now}, 'time.start':{$lte:now}}, {'time.start':{$gte:now, $lte:oneweek}}]}).sort('time.start'd).populate('venue').exec(function(err, events){
+	Event.find({$or:[{'time.end':{$gte:now}, 'time.start':{$lte:now}}, {'time.start':{$gte:now, $lte:oneweek}}]}).sort('time.start').populate('venue').exec(function(err, events){
 		if (err) {
 			res.send(500, 'Error #301: '+err);
 		}else{
