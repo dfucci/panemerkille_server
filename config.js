1612029217c7a6ae58630d428c0e173d4d078edf
Daniele Zanni <syymza@gmail.com>
2012-09-24 18:15:30 +0300

    config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ var config = {};
 config.restify={};
 config.mongo={};
 config.restify.port = process.env.PORT || 7777;
-config.mongo.url = process.env.MONGOLAB_URI || "mongodb://localhost/heroku_app3697279";
+config.mongo.url = process.env.MONGOLAB_URI || "mongodb://localhost/pm20120924";
 module.exports = config;
