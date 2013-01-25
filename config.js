d08cff0e890c566d8d917f1afc169e6bf7de1260
slacko <ancestrall@gmail.com>
2013-01-25 17:41:05 +0200

    config

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ config.restify={};
 config.mongo={};
 config.restify.port = process.env.PORT || 7777;
 //config.mongo.url = process.env.MONGOLAB_URI || "mongodb://localhost/heroku_app3697279";
-config.mongo.url = "mongodb://panemerkille:da85da85te84@ds049157.mongolab.com:49157/panemerkille-stagin";
+config.mongo.url = "";
 module.exports = config;
