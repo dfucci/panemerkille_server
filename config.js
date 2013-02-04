50faf9a7277fa942171f130d9379d61b7ca81483
slacko <ancestrall@gmail.com>
2013-02-04 19:17:21 +0200

    fixed config

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ var config = {};
 config.restify={};
 config.mongo={};
 config.restify.port = process.env.PORT || 7777;
-//config.mongo.url = process.env.MONGOLAB_URI || "mongodb://localhost/heroku_app3697279";
-config.mongo.url = "";
+config.mongo.url = process.env.MONGOLAB_URI || "mongodb://localhost/heroku_app3697279";
 module.exports = config;
